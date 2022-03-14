5cfe39cbab2bfc8e2920eafc58f5f6f08b01da18
Личак Михаил <mihail.lichak@gmail.com>
2022-03-14 13:00:09 +0300

    examples

---
+++ examples/other.java
@@ -0,0 +1,113 @@
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+public class BubbleSorter {
+    public static void sort(int[] array) {
+        // i - номер прохода
+        for (int i = 0; i < array.length - 1; i++) {
+            // внутренний цикл прохода
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+}
+
+public class LongestPalindromeString {
+    static public String intermediatePalindrome(String s, int left, int right) {
+        if (left > right) return null;
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+    // O(n^2)
+    public static String longestPalindromeString(String s) {
+        if (s == null) return null;
+        String longest = s.substring(0, 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            //odd cases like 121
+            String palindrome = intermediatePalindrome(s, i, i);
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
+            }
+            //even cases like 1221
+            palindrome = intermediatePalindrome(s, i, i + 1);
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
+            }
+        }
+        return longest;
+    }
+}
+
+public class isPalindromeNumber{
+    int num = 3553, reversedNum = 0, remainder;
+
+    // store the number to originalNum
+    int originalNum = num;
+
+    // get the reverse of originalNum
+    // store it in variable
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    // check if reversedNum and originalNum are equal
+    if (originalNum == reversedNum) {
+        System.out.println(originalNum + " is Palindrome.");
+    }
+    else {
+        System.out.println(originalNum + " is not Palindrome.");
+    }
+}
+
+public class longestUnrepeatableSubstring{
+    public int lengthOfLongestSubstring(String s) {
+        Integer[] chars = new Integer[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+
+            Integer index = chars[r];
+            if (index != null && index >= left && index < right) {
+                left = index + 1;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            chars[r] = right;
+            right++;
+        }
+
+        return res;
+}
